a4acefb4a0967c2e103c5e0115e26e1af84e7869
Bocharick <pomazannn@gmail.com>
2019-02-04 16:35:11 +0700

    lesson 02 done

--- project/server/mainapp/views.py
+++ project/server/mainapp/views.py
@@ -1,3 +1,6 @@
+from django.http import HttpResponse
+from django.template import Template, Context
+from django.template.loader import render_to_string, get_template
 from django.shortcuts import render
 
 # Create your views here.
@@ -14,7 +17,24 @@ def about(request):
     )
 
 def contacts(request):
-    return render(
-        request,
-        'mainapp/contacts.html'
+    rendered_page = render_to_string(
+        'mainapp/contacts.html',
+        {
+            'contacts': [
+                'Контакт 1',
+                'Контакт 2',
+                'Контакт 3',
+                'Контакт 4',
+                'Контакт 5',
+                'Контакт 6',
+                'Контакт 7',
+            ]
+        }
     )
+    return HttpResponse(
+        rendered_page
+    )
+    # return render(
+    #     request,
+    #     'mainapp/contacts.html'
+    # )
--- project/server/server/settings.py
+++ project/server/server/settings.py
@@ -58,7 +58,9 @@ ROOT_URLCONF = 'server.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'server', 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,3 +124,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'server', 'static')
+]
